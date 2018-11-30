27fe7ed051a4a06f77c540f84051e2554dfa791f
Sam Petering <30877125+SamPetering@users.noreply.github.com>
2018-11-30 10:52:17 -0600

    Add files via upload

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string to reverse: ");
+            Console.WriteLine(reverse(Console.ReadLine()));
+            Console.ReadLine();
+        }
+        public static string reverse(string str)
+        {
+            string rev = "";
+            char[] arr = str.ToCharArray();
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                rev += arr[i];
+            }
+            return rev;
+        }
+    }
+}
