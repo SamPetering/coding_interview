71135e12394173ce7d4f8e8e0fbe9005878d2ef3
Sam Petering <30877125+SamPetering@users.noreply.github.com>
2018-12-04 13:57:43 -0600

    Add files via upload

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack myStack = new Stack();
+            myStack.push(5);
+            myStack.push(10);
+            myStack.push(15);
+            myStack.push(20);
+            while (!myStack.isEmpty())
+            {
+                Console.WriteLine(myStack.pop());
+            }
+            Console.ReadKey();
+        }
+    }
+
+    class Stack
+    {
+        private class Node
+        {
+            public int data;
+            public Node next;
+            public Node(int data)
+            {
+                this.data = data;
+            }
+        }
+
+        private Node top;
+
+        public bool isEmpty()
+        {
+            return top == null;
+        }
+
+        public int peek()
+        {
+            if (isEmpty()) return -1;
+            return top.data;
+        }
+
+        public void push(int data)
+        {
+            Node node = new Node(data);
+            node.next = top;
+            top = node;
+        }
+
+        public int pop()
+        {
+            int data = top.data;
+            top = top.next;
+            return data;
+        }
+    }
+}
