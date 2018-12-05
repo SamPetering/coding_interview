57f6c66ecd2dbd573341fed5fab77dd8d688a190
Sam Petering <30877125+SamPetering@users.noreply.github.com>
2018-12-04 20:48:38 -0600

    Add files via upload

---
+++ FibMemoized/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibMemoized
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 20;
+            int[] A = new int[n + 1];
+            Console.WriteLine(fib(n, A));
+            Console.ReadKey();
+        }
+
+        static int fib(int n, int[] memo)
+        {
+            if (memo[n] != 0) return memo[n];
+            int result;
+            if (n == 1 || n == 2)
+                result = 1;
+            else
+                result = fib(n - 1, memo) + fib(n - 2, memo);
+            memo[n] = result;
+            return result;
+        }
+    }
+}
